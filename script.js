21f0286e3620e3fe05f50e22d4fb4ffd0c4f1e1f
Johnatan <jonystrit@gmail.com>
2021-05-25 09:52:04 -0300

    Testando a push

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var singIn = document.getElementById("sing-in")
 var singUp = document.getElementById("sing-up")
 var body = document.getElementById("body")
+var teste = "TESTANDO O GIT" //TESTE DO Git
 
 singIn.addEventListener("click", function(){
     body.className = "sing-in-js"
