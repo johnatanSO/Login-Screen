01159b8fb9e6808d26ac1555ff7640184426e65a
Johnatan Santos <jonystrit@gmail.com>
2021-11-29 13:05:43 -0300

    js update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var singIn = document.getElementById("sing-in")
 var singUp = document.getElementById("sing-up")
 var body = document.getElementById("body")
-var teste = "TESTANDO O GIT" //TESTE DO Git
 
 singIn.addEventListener("click", function(){
     body.className = "sing-in-js"
--- teste.js
+++
@@ -1,5 +0,0 @@
-numeros = ["A","B","C","D","E","F"]
-
-for (var i = 0; i < numeros.length; i++){
-    console.log(numeros[i])
-}
